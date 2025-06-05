ee23adff3830a9fb4dbabfda5edf5bcc2ab7218f
alander110 <wy1366368016@gmail.com>
2025-06-05 21:25:57 +0800

    admin远程调用project方法

--- admin/src/main/java/com/xw/shortlink/admin/AdminApplication.java
+++ admin/src/main/java/com/xw/shortlink/admin/AdminApplication.java
@@ -3,9 +3,11 @@ package com.xw.shortlink.admin;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @MapperScan("com.xw.shortlink.admin.dao.mapper")
+@EnableFeignClients("com.xw.shortlink.admin.remote")
 public class AdminApplication {
 
     public static void main(String[] args) {
